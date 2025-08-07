3d0098a9a2713bfae3a5e2fb48262d9ff2825c67
WaffleSpaghetti <135555461+WaffleSpaghetti@users.noreply.github.com>
2025-08-07 19:50:55 +0300

    Update example.lua
    its more readable now

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local LSystem = require(script.LSystem)
 local Turtle = require(script.Turtle)
 
-local obj1 = LSystem{
+local plant1 = LSystem{
 	axiom = "A",
 	rules = {
 		A = {
@@ -10,33 +10,43 @@ local obj1 = LSystem{
 		},
 	},
 }
-local obj2 = LSystem{
-	axiom = "PF",
-	rules = {
-		F = "FF",
-	},
-}
-
-print(obj2:run(4))
 
 local folder = Instance.new("Folder")
+folder.Parent = workspace
 local t1 = Turtle{
 	pos = Vector3.new(0,10,-10),
 	container = folder,
 }
 
-t1:build(obj1:run(10,false))
+local function growPlant(plant, turtle, pos, color, steps)
+	turtle.pos = pos
+	turtle.color = color
+	turtle:build(plant:run(steps,false))
+end
 
-t1.pos = Vector3.new(-10,10,-10)
-t1.color = Color3.new(0,0,1)
-t1:build(obj1:run(15,false))
-folder.Parent = workspace
+
+growPlant(plant1, t1, Vector3.new(-20,10,10), Color3.new(1,0,0),5)
+growPlant(plant1, t1, Vector3.new(-10,10,10), Color3.new(0,1,0),10)
+growPlant(plant1, t1, Vector3.new(0,10,10), Color3.new(0,0,1),15)
+growPlant(plant1, t1, Vector3.new(10,10,10), Color3.new(0,1,0),10)
+growPlant(plant1, t1, Vector3.new(20,10,10), Color3.new(1,0,0),5)
 
 
+
+local plant2 = LSystem{
+	axiom = "PG",
+	rules = {
+		G = {
+			["FG"] = 1,
+			["[xF][XF][zF][ZF]F"] = 3,
+		},
+		F = "FFG",
+	},
+}
 
 local model = Instance.new("Model")
-t1.pos = Vector3.new(10,10,-10)
-t1.container = model
-t1.color = Color3.new(0,1,0)
-t1:build(obj2.string)
 model.Parent = workspace
+t1.container = model
+t1.turnAngle = math.rad(15)
+
+growPlant(plant2, t1, Vector3.new(0,10,20), Color3.new(1,1,1),5)
