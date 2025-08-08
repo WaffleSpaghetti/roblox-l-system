30a7194d841df8d2a4bb47bf451aa9e3c1bf2092
WaffleSpaghetti <135555461+WaffleSpaghetti@users.noreply.github.com>
2025-08-08 14:55:43 +0300

    Update treeModule.lua
    its cleaner and more readable now

--- treeModule.lua
+++ treeModule.lua
@@ -1,7 +1,7 @@
 local LSystem = require(game.ReplicatedStorage.LSystem)
 local Turtle = require(game.ReplicatedStorage.Turtle)
 
-local function choose(colors)
+local function chooseColor(colors)
 	local accumulator = 0
 	for _, weight in pairs(colors) do
 		accumulator += weight
@@ -20,7 +20,7 @@ trees.plants = {
 		rules = {
 			F = "G[[XvF]xvF][ZvF]zvF",
 			G = {
-				["G"] = 1,
+				["G"] = 3,
 				["GG"] = 1,
 			},
 		}
@@ -35,45 +35,30 @@ trees.turtles = {
 				self.lineWidth /= 1.5
 				self.speed /= 1.2
 			end,
-			
 			["F"] = function(self)
-				self.pos = self.pos + self.dir.Unit * self.speed/2
-				
 				local colors = {
 					["#669900"] = 10,
 					["#00FF00"] = 10,
 					["#FF0000"] = 1,
 				}
+				local pickedColor = chooseColor(colors)
 				
-				local part = Instance.new("Part")
-				part.Size = Vector3.one * 1.5
-				part.CanCollide=self.canCollide
-				part.CastShadow=self.castShadow
-				part.Material=self.material
-				part.Transparency=self.transparency
-				part.Anchored = true
-				local pickedColor = choose(colors)
-				part.Color = Color3.fromHex(pickedColor)
-				part.CFrame = CFrame.lookAt(self.pos, self.pos + self.dir)
-				part.Parent = self.container
-
-				self.pos = self.pos + self.dir.Unit * self.speed/2
+				self:move(0.5)
+				self:buildBranch(
+					CFrame.lookAt(self.pos, self.pos + self.dir),
+					Vector3.one,
+					Color3.fromHex(pickedColor)
+				)
+				self:move(0.5)
 			end,
 			["G"] = function(self)
-				self.pos = self.pos + self.dir.Unit * self.speed/2
-				
-				local part = Instance.new("Part")
-				part.Size = Vector3.new(self.lineWidth,self.lineWidth,self.speed)
-				part.CanCollide=self.canCollide
-				part.CastShadow=self.castShadow
-				part.Material=self.material
-				part.Transparency=self.transparency
-				part.Anchored = true
-				part.Color = Color3.new(0.55,0.35,0)
-				part.CFrame = CFrame.lookAt(self.pos, self.pos + self.dir)
-				part.Parent = self.container
-
-				self.pos = self.pos + self.dir.Unit * self.speed/2
+				self:move(0.5)
+				self:buildBranch(
+					CFrame.lookAt(self.pos, self.pos + self.dir),
+					Vector3.new(self.lineWidth,self.lineWidth,self.speed),
+					Color3.new(0.55,0.35,0)
+				)
+				self:move(0.5)
 			end,
 		},
 	}
@@ -88,17 +73,5 @@ function trees:growApple(pos,steps,container)
 	turtle.pos = pos
 	turtle:build(plant:run(steps,false))
 end
-
---[[
-function trees:growSakura(pos,steps,container)
-	local plant = self.plants.sakuraTree
-	local turtle = self.turtles.sakuraTurtle
-	
-	turtle.container = container
-	turtle:reset()
-	turtle.pos = pos
-	turtle:build(plant:run(steps,false))
-end
-]]
 
 return trees
