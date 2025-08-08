407aeb8e5a0def371efa6b9114cf675109bc7b27
WaffleSpaghetti <135555461+WaffleSpaghetti@users.noreply.github.com>
2025-08-08 15:29:22 +0300

    Update forest.lua

--- forest.lua
+++ forest.lua
@@ -4,9 +4,9 @@ local Trees = require(game.ReplicatedStorage.Trees)
 local size = 60
 for x=-size,size,20 do
 	for z=-size,size,20 do
-		Trees:growApple(Vector3.new(x,10,z),5)
+		Trees:growApple(Vector3.new(x,10,z),Vector3.new(0,1,0),5)
 	end
 end
 --]]
 
-Trees:growApple(Vector3.new(0,10,-120),8)
+Trees:growApple(Vector3.new(0,10,-120),Vector3.new(0,1,0),8)
